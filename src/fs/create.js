4edbcdb3e3c17d52143e70583fcfccaad2f47773
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-06-01 23:27:40 +0300

    feat: fix create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -2,11 +2,10 @@ import path from "path";
 import fsPromises from "fs/promises";
 import fs from "fs";
 
-const OUTPATH = path.join(path.resolve(), "files", "fresh.txt");
-const TEXTFORWRITE = "I am fresh and young";
-const TEXTFORERROR = "FS operation failed";
-
 export const create = async () => {
+    const OUTPATH = path.join(path.resolve(), "files", "fresh.txt");
+    const TEXTFORWRITE = "I am fresh and young";
+    const TEXTFORERROR = "FS operation failed";
     fs.stat(OUTPATH, async (err) => {
         if (err == null) {
             throw new Error(TEXTFORERROR);
