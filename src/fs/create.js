71511a05edbc06edb9af5326158d1b144e772b8e
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-06-01 23:24:08 +0300

    feat: complete create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,19 @@
+import path from "path";
+import fsPromises from "fs/promises";
+import fs from "fs";
+
+const OUTPATH = path.join(path.resolve(), "files", "fresh.txt");
+const TEXTFORWRITE = "I am fresh and young";
+const TEXTFORERROR = "FS operation failed";
+
 export const create = async () => {
-    // Write your code here 
+    fs.stat(OUTPATH, async (err) => {
+        if (err == null) {
+            throw new Error(TEXTFORERROR);
+        } else {
+            await fsPromises.writeFile(OUTPATH, TEXTFORWRITE);
+        }
+    });
 };
+
+create();
