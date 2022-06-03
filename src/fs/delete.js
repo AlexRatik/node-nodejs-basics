6cf74cbe40dc35ccd28b2efb7752118a2ebfe660
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-06-04 00:07:12 +0300

    feat: complete delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,16 @@
+import path from "path";
+import fsPromises from "fs/promises";
+
 export const remove = async () => {
-    // Write your code here 
+    const TEXTFORERROR = "FS operation failed";
+    const pathForDelete = path.join(
+        path.resolve(),
+        "files",
+        "fileToRemove.txt"
+    );
+    fsPromises.rm(pathForDelete).catch(() => {
+        throw new Error(TEXTFORERROR);
+    });
 };
+
+remove();
