ab8de409c8c9852467b10d2bc0aa49cabb527fc3
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-06-04 00:16:46 +0300

    feat: complete list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,23 @@
+import path from "path";
+import fsPromises from "fs/promises";
+
 export const list = async () => {
-    // Write your code here 
+    const TEXTFORERROR = "FS operation failed";
+    const dirForPrint = path.join(path.resolve(), "files");
+    fsPromises
+        .access(dirForPrint)
+        .catch(() => {
+            throw new Error(TEXTFORERROR);
+        })
+        .then(() => {
+            return fsPromises.readdir(dirForPrint);
+        })
+        .then((files) => {
+            if (files.length === 0) {
+                console.log("\n\tThere are no files!!!\n");
+            }
+            files.forEach((file) => console.log("\t" + file + "\n"));
+        });
 };
+
+list();
