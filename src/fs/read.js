5544f82ec814b1e0a81e9055af112e3a0664ebef
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-06-04 00:21:36 +0300

    feat: complete read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,20 @@
+import path from "path";
+import fsPromises from "fs/promises";
+
 export const read = async () => {
-    // Write your code here 
+    const TEXTFORERROR = "FS operation failed";
+    const pathForRead = path.join(path.resolve(), "files", "fileToRead.txt");
+    fsPromises
+        .access(pathForRead)
+        .catch(() => {
+            throw new Error(TEXTFORERROR);
+        })
+        .then(async () => {
+            return await fsPromises.readFile(pathForRead, {
+                encoding: "utf-8",
+            });
+        })
+        .then(console.log);
 };
+
+read();
