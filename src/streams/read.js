6e8ca22c5dedc0d6a8db7921a44b96d77c177d18
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-06-04 17:23:13 +0300

    feat: fix read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,12 +1,11 @@
 import path from "node:path";
 import fs from "node:fs";
+import { stdout } from "node:process";
 
 export const read = async () => {
     const fileToRead = path.join(path.resolve(), "files", "fileToRead.txt");
     const stream = fs.createReadStream(fileToRead, { encoding: "utf-8" });
-    stream.on("data", (data) => {
-        console.log("\n\n" + data);
-    });
+    stream.pipe(stdout);
 };
 
 read();
