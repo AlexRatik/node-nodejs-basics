de65110433c1609c9ad7d4bce40ae20d6d96be34
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-06-04 17:19:56 +0300

    feat: complete read.js task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,12 @@
+import path from "node:path";
+import fs from "node:fs";
+
 export const read = async () => {
-    // Write your code here 
+    const fileToRead = path.join(path.resolve(), "files", "fileToRead.txt");
+    const stream = fs.createReadStream(fileToRead, { encoding: "utf-8" });
+    stream.on("data", (data) => {
+        console.log("\n\n" + data);
+    });
 };
+
+read();
