d736565da51218f774b671374578dd2b0c5a4912
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-06-04 17:13:02 +0300

    feat: complete calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,18 @@
+import path from "node:path";
+import fsPromises from "node:fs/promises";
+import { createHash } from "node:crypto";
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const fileForHash = path.join(
+        path.resolve(),
+        "files",
+        "fileToCalculateHashFor.txt"
+    );
+    await fsPromises
+        .readFile(fileForHash, { encoding: "utf-8" })
+        .then((text) => {
+            console.log(createHash("sha256").update(text).digest("hex"));
+        });
 };
+
+calculateHash();
