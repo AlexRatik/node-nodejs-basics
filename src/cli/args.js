4e863b9ff261a17d440e57e29842076d62ff0906
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-06-04 15:48:51 +0300

    feat: complete args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,18 @@
 export const parseArgs = () => {
-    // Write your code here 
+    let result = "";
+    const args = process.argv.slice(2);
+    args.map((arg, i) => {
+        if (i % 2 === 0) {
+            result += `${arg.slice(2)} is `;
+        } else {
+            if (i === args.length - 1) {
+                result += arg;
+            } else {
+                result += `${arg}, `;
+            }
+        }
+    });
+    console.log(result);
 };
+
+parseArgs();
