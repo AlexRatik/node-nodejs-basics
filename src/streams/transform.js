a64d61f39c5394d8a805c883752c14dcf2401946
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-06-05 19:08:12 +0300

    feat: complete transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,23 @@
+import path from "node:path";
+import fs from "node:fs";
+import { stdout } from "node:process";
+import { Stream } from "node:stream";
+
 export const transform = async () => {
-    // Write your code here 
+    const fileToRead = path.join(path.resolve(), "files", "fileToRead.txt");
+    const fileToWrite = path.join(path.resolve(), "files", "fileToWrite.txt");
+    const readableStram = fs.createReadStream(fileToRead, {
+        encoding: "utf-8",
+    });
+    const writeableStream = fs.createWriteStream(fileToWrite, {
+        encoding: "utf-8",
+    });
+    const transform = Stream.Transform({
+        transform(chunk, options, cb) {
+            cb(null, chunk.toString().split("").reverse().join(""));
+        },
+    });
+    readableStram.pipe(transform).pipe(writeableStream);
 };
+
+transform();
