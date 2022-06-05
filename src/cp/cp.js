2cf4bcc939ebeddeeef6556c73e0d04fb9c4a0e0
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-06-05 20:30:07 +0300

    feat: complete cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,9 @@
+import path from "node:path";
+import { fork } from "node:child_process";
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childFile = path.join(path.resolve(), "files", "script.js");
+    fork(childFile, args);
 };
+
+spawnChildProcess([5, 1, 9]);
