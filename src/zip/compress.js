b3a65fac45d049bbf73039cc194381e285419e8f
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-06-05 19:35:10 +0300

    feat: complete compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,23 @@
+import path from "node:path";
+import zlib from "node:zlib";
+import fs from "node:fs";
+
 export const compress = async () => {
-    // Write your code here 
+    const fileForCompress = path.join(
+        path.resolve(),
+        "files",
+        "fileToCompress.txt"
+    );
+    const fileForWrite = path.join(path.resolve(), "files", "archive.gz");
+    const gzip = zlib.createGzip();
+    const readStream = fs.createReadStream(fileForCompress);
+    const writeStream = fs.createWriteStream(fileForWrite);
+    readStream.pipe(gzip).pipe(writeStream);
+    fs.rm(fileForCompress, (err) => {
+        if (err) {
+            console.log(err.name);
+        }
+    });
 };
+
+compress();
